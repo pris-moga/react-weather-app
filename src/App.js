b4401948697aa7b61c6f47d1f54b5b9837589d07
Priscilla Morales <90214178+pris-moga@users.noreply.github.com>
2022-01-08 15:20:17 -0600

    Added the footer to App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
-        <h1>Weather App</h1>
+      <div className="back-rectangle-one">
+        <div className="container">
+          <h1>Weather App</h1>
+        </div>
       </div>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.behance.net/P-MOGA"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Priscilla Morales
+        </a>{" "}
+        and is{" "}
+        <a href="/" target="_blank" rel="noreferrer">
+          {" "}
+          open-sourced on GitHub.
+        </a>{" "}
+        Hosted on Netlify.
+      </footer>
     </div>
   );
 }
