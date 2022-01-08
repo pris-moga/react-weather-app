0283078a4050dcf7006420e2d5ce7b30ee952622
Priscilla Morales <90214178+pris-moga@users.noreply.github.com>
2022-01-08 15:05:34 -0600

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,5 +2,11 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+      </div>
+    </div>
+  );
 }
