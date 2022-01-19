ecaec8881c8d581e2a595ebb9c6ec41f2ed0e833
Priscilla Morales <90214178+pris-moga@users.noreply.github.com>
2022-01-18 22:43:22 -0600

    Added 5 days to the daily forecast with a loop

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -18,9 +18,15 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col forecast-container">
-            <ForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col forecast-container" key={index}>
+                  <ForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
